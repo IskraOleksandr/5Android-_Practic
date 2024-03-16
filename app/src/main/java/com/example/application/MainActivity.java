0f87f14dad4ffdb35926731482989ec93f5db977
_Iskra_ <_Iskra_@DESKTOP-4SUHQ4F>
2024-03-16 18:20:43 +0200

    #Task Создание вертикального интерфейса калькулятора

---
+++ app/src/main/java/com/example/application/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//сделать интерфейс инженерного калькулятора для альбомної орієнтації
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
